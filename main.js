d4b30f5846e5de4f8d208cc6d8808400be21456c
giorgiospirostopala <gio.topala@gmail.com>
2025-01-21 00:55:38 +0100

    add: preparazione html

---
+++ main.js
@@ -0,0 +1,14 @@
+/* Consegna
+• Inseriamo in pagina l’immagine della lampadina spenta che trovate in allegato
+• accanto un bottone con la scritta “Accendi”.
+
+Al click del bottone, la lampadina dovrà accendersi (switch foto lampadina accesa)
+
+BONUS
+Al primo click la lampadina si accende e nel bottone compare la scritta "Spegni"
+Al secondo click la lampadina si spegne e nel bottone compare la scritta "Accendi"
+E così via...
+usa string.includes()  */
+
+
+
